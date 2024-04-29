ea0f3710e43e54d8013ccbc248e3933d74ffe631
atharvaaalok <atharvaaalok@gmail.com>
2024-04-29 08:02:18 +0530

    Added SGD optimizer.

---
+++ Components/optimizers/sgd.py
@@ -0,0 +1,10 @@
+from optimizer import Optimizer
+
+
+class SGD(Optimizer):
+
+    def __init__(self, data_shape):
+        super().__init__(data_shape)
+    
+    def update(self, data_deriv):
+        return data_deriv
