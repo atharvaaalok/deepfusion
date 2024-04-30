def3e74e5150da1e6c1a02abd9e88aab6e6e4f5c
atharvaaalok <atharvaaalok@gmail.com>
2024-04-30 20:30:05 +0530

    For the optimizer and sgd modules, added docstrings and type hints and made the code fall within a line width of 100 characters.

--- components/optimizers/optimizer.py
+++ components/optimizers/optimizer.py
@@ -1,11 +1,26 @@
 from abc import ABC, abstractmethod
+import numpy.typing as npt
 
 
 class Optimizer(ABC):
+    """Base class from which all optimizers should inherit.
+    
+    Attributes:
+        data_shape: Shape of the Data object that the optimizer is associated with.
+    """
 
-    def __init__(self, data_shape):
+    def __init__(self, data_shape: tuple[int, ...]) -> None:
         self.data_shape = data_shape
     
+    
     @abstractmethod
-    def step(self, data_deriv):
+    def step(self, data_deriv: npt.NDArray) -> npt.NDArray:
+        """Returns the update tensor for the Data object given its derivative.
+        
+        Args:
+            data_deriv: Derivative of the Data object at its current value for some loss function.
+        
+        Returns:
+            np.ndarray: Returns a tensor to be used by the Data object for performing an update.
+        """
         pass
--- components/optimizers/sgd.py
+++ components/optimizers/sgd.py
@@ -1,10 +1,14 @@
 from .optimizer import Optimizer
+from typing import override
+import numpy.typing as npt
 
 
 class SGD(Optimizer):
 
-    def __init__(self, data_shape):
+    def __init__(self, data_shape: tuple[int, ...]) -> None:
         super().__init__(data_shape)
     
-    def step(self, data_deriv):
+
+    @override
+    def step(self, data_deriv: npt.NDArray) -> npt.NDArray:
         return data_deriv
