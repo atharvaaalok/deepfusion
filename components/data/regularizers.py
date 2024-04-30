83e5f95836e23386c60b92c47465f588eeb38666
atharvaaalok <atharvaaalok@gmail.com>
2024-04-30 16:58:08 +0530

    For the regularizer module, added docstrings and type hints and made the code fall within a line width of 100 characters.

--- components/data/regularizers.py
+++ components/data/regularizers.py
@@ -1,28 +1,50 @@
 import numpy as np
+import numpy.typing as npt
+from typing import Callable
 
 
 class Regularizer:
+    """Object holding the regularization strength and function and its derivative.
 
-    def __init__(self, regularizer_details):
+    Attributes:
+        reg_strength: Regularization strength that scales the contribution to the loss.
+        reg_name: Name of the regularizer to be used. Eg: 'L2'
+        reg_fn: Regularization function.
+        reg_fn_deriv: Derivative of the regularization function.
+    """
+
+    def __init__(self, regularizer_details: dict):
         self.reg_strength = regularizer_details['reg_strength']
         self.reg_name = regularizer_details['reg_name']
         self.reg_fn, self.reg_fn_deriv = _get_regularizer_with_deriv(self.reg_name)
 
 
-def _get_regularizer_with_deriv(regularizer_name):
+def _get_regularizer_with_deriv(regularizer_name: str) -> tuple[Callable, Callable]:
+    """For the specified regularizer returns the regularization function and its derivative.
+    
+    Args:
+        regularizer_name: String chosen from the available regularization choices.
+    
+    Returns:
+        (fn, fn_deriv): A tuple with handles to the regularization function and its derivative.
+    
+    Raises:
+        ValueError: If the specified regularizer is not available.
+    """
 
     try:
         return _regularizers_with_deriv_dict[regularizer_name]
     except KeyError:
-        raise ValueError(f'Specified regularizer not available. Choose from - {list(_regularizers_with_deriv_dict)}')
+        raise ValueError('Specified regularizer not available.' \
+                         f' Choose from - {list(_regularizers_with_deriv_dict)}')
 
 
 
-def _L2_regularizer(x):
+def _L2_regularizer(x: npt.NDArray) -> npt.NDArray:
     return np.sum(x ** 2)
 
 
-def _L2_regularizer_deriv(x):
+def _L2_regularizer_deriv(x: npt.NDArray) -> npt.NDArray:
     return 2 * x
 
 
