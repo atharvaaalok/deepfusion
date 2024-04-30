4472ec4f1838ebfe29ad78caceb4d123659d7231
atharvaaalok <atharvaaalok@gmail.com>
2024-04-30 11:45:29 +0530

    Added regularization to matmul module.

--- components/modules/matmul.py
+++ components/modules/matmul.py
@@ -11,10 +11,13 @@ class MatMul(Module):
     is_regularizable = True
 
 
-    def __init__(self, ID, inputs, output, learning_rate = 1e-6, is_frozen = False, optimizer_details = DEFAULT_OPTIMIZER_DETAILS, is_regularized = False, regularizer_details = None, weight_init_type = 'He'):
+    def __init__(self, ID, inputs, output, learning_rate = 1e-6, is_frozen = False, optimizer_details = DEFAULT_OPTIMIZER_DETAILS, is_regularized = False, regularizer_details = None, weight_init_type = 'He'):        
 
         if is_regularized and regularizer_details is None:
             raise ValueError('If Module is regularized, regularizer details must be provided.')
+        
+        self.is_regularized = is_regularized
+        self.regularizer_details = regularizer_details if is_regularized else None
         
         W_shape = (output.shape[0], inputs[0].shape[0])
         b_shape = (output.shape)
@@ -38,6 +41,11 @@ class MatMul(Module):
         self.W.deriv = (1 / batch_size) * (self.output.deriv @ self.inputs[0].val.T)
         self.b.deriv = (1 / batch_size) * (np.sum(self.output.deriv, axis = 1, keepdims = True))
 
+    
+    def set_regularization(self, regularizer_details):
+        self.is_regularized = True
+        self.regularizer_details = regularizer_details
+        self.W.set_regularization(regularizer_details)
 
 
 def _weight_initialization(weight_init_type, W_shape):
