a2348d0e0a1f8b97574eab5a236325f2e9568336
atharvaaalok <atharvaaalok@gmail.com>
2024-04-30 22:29:42 +0530

    For the Relu module, added docstrings and type hints and made the code fall within a line width of 100 characters.

--- components/modules/activation_functions/relu.py
+++ components/modules/activation_functions/relu.py
@@ -1,21 +1,44 @@
 import numpy as np
+from typing import override
 
 from ..module import Module
+from ...data.data import Data
 
 
 class Relu(Module):
+    """Rectified Linear Unit (ReLU) module that given an input X computes max(0, X) element-wise.
+    
+    Attributes:
+        ID:
+            A unique string identifier for the Module object.
+        inputs:
+            List of Data objects whose values the module transforms to produce an output.
+        output:
+            Data object which stores the module's output after transforming the input values.
+        parameter_list:
+            Not used by the module. Set to default value by the Module base class.
+        learning_rate:
+            Not used by the module. Set to default value by the Module base class.
+        is_frozen:
+            Not used by the module. Set to default value by the Module base class.
+        optimizer_details:
+            Not used by the module. Set to default value by the Module base class.
+    """
 
     different_at_train_test = False
     is_regularizable = False
 
 
-    def __init__(self, ID, inputs, output):
+    def __init__(self, ID: str, inputs: list[Data], output: Data) -> None:
+        """Initializes the Relu module based on ID, inputs and output."""
         super().__init__(ID, inputs, output)
     
 
-    def forward(self):
+    @override
+    def forward(self) -> None:
         self.output.val = np.maximum(self.inputs[0].val, 0.0)
     
 
-    def backward(self):
+    @override
+    def backward(self) -> None:
         self.inputs[0].deriv = self.output.deriv * np.where(self.inputs[0].val >= 0.0, 1.0, 0.0)
