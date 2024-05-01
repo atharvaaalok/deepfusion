36d19b7b69e475620c0d607fac75af89b29330c2
atharvaaalok <atharvaaalok@gmail.com>
2024-05-01 17:36:38 +0530

    Added docstring for SGD optimizer.

--- components/optimizers/sgd.py
+++ components/optimizers/sgd.py
@@ -4,6 +4,11 @@ import numpy.typing as npt
 
 
 class SGD(Optimizer):
+    """Stochastic Gradient Descent (SGD) optimizer that performs update with plain loss derivative.
+    
+    Attributes:
+        data_shape: Shape of the Data object that the optimizer is associated with.
+    """
 
     def __init__(self, data_shape: tuple[int, ...]) -> None:
         super().__init__(data_shape)
