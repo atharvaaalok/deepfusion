4aa8e58c861e8765299ae9418588a428f9cc51ea
atharvaaalok <atharvaaalok@gmail.com>
2024-05-03 12:04:26 +0530

    Added Inverted Dropout as the first dropout module!

---
+++ components/modules/dropout/inverted_dropout.py
@@ -0,0 +1,69 @@
+import numpy as np
+from typing import override
+
+from ..module import Module
+from ...data.data import Data
+
+
+class InvertedDropout(Module):
+    """Inverted Dropout module that drops some neurons at random during forward pass in training.
+
+    Attributes:
+        ID:
+            A unique string identifier for the Module object.
+        inputs:
+            List of Data objects whose values the module transforms to produce an output.
+        output:
+            Data object which stores the module's output after transforming the input values.
+        parameter_list:
+            Not used by the module. Set to default value by the Module base class.
+        learning_rate:
+            Not used by the module. Set to default value by the Module base class.
+        is_frozen:
+            Not used by the module. Set to default value by the Module base class.
+        optimizer_details:
+            Not used by the module. Set to default value by the Module base class.
+        mode:
+            Module works differently in train and test. Maintain state to use appropriate values.
+        p_keep:
+            Probability of keeping a particular neuron during forward pass in training.
+        cache:
+            Dictionary that stores values during forward pass that will be used in backward pass.
+    """
+
+    different_at_train_test = True
+    is_regularizable = False
+
+    
+    def __init__(self, ID: str, inputs: list[Data], output: Data, p_keep: float = 0.5) -> None:
+        """Initialize the inverted dropout module based on ID, inputs, output and optional
+        parameters."""
+
+        super().__init__(ID, inputs, output)
+        
+        # Works differently in train and test mode. Maintain state to use appropriate values
+        self.mode = 'train'
+        # Probability of keeping a particular neuron during forward pass in training
+        self.p_keep = p_keep
+        # Maintain cache for scaled mask used in forward pass during training
+        self.cache = {'mask': 0}
+    
+
+    @override
+    def forward(self) -> None:
+        if self.mode == 'train':
+            # Drop neurons at randon based on probability p_keep and scale up the values
+            mask = (np.random.rand(*self.inputs[0].val.shape) < self.p_keep) / self.p_keep
+            # Apply the mask
+            self.output.val = self.inputs[0].val * mask
+            # Cache the mask to use it during the backward pass
+            self.cache['mask'] = mask
+
+        elif self.mode == 'test':
+            # Don't apply mask, forward the input values unaltered
+            self.output.val = self.inputs[0].val
+
+
+    @override
+    def backward(self) -> None:
+        self.inputs[0].deriv = self.output.deriv * self.cache['mask']
