eebb8c1cc9c464584125e0bb17f343e0e46d2a7a
atharvaaalok <atharvaaalok@gmail.com>
2024-05-13 00:01:44 +0530

    Added Flatten module. It flattens each training example so that it can be fed to a FNN.

--- components/modules/__init__.py
+++ components/modules/__init__.py
@@ -1,3 +1,4 @@
 from .module import Module
 from .matmul import MatMul
 from .add import Add
+from .flatten import Flatten
---
+++ components/modules/flatten.py
@@ -0,0 +1,57 @@
+import numpy as np
+from typing import override
+
+from .module import Module
+from ..data.data import Data
+
+
+class Flatten(Module):
+    """Flatten takes any tensor of shape (a, b, c, ..., m) where m is batch size and converts it
+    into a 2D matrix of size (a * b * c * ..., m).
+
+    Attributes:
+        ID:
+            A unique string identifier for the Module object.
+        inputs:
+            List of Data objects whose values the module transforms to produce an output.
+        output:
+            Data object which stores the module's output after transforming the input values.
+        parameter_list:
+            Not used by the module. Set to default value by the Module base class.
+        learning_rate:
+            Not used by the module. Set to default value by the Module base class.
+        is_frozen:
+            Not used by the module. Set to default value by the Module base class.
+        optimizer_details:
+            Not used by the module. Set to default value by the Module base class.
+    """
+
+    different_at_train_test = False
+    is_regularizable = False
+
+
+    def __init__(self, ID: str, inputs: list[Data], output: Data) -> None:
+        """Initializes Flatten module based on ID, inputs and output."""
+
+        # Go through checks first
+        total_input_elements = np.prod(inputs[0].shape)
+        total_output_elements = output.shape[0]
+        assert total_input_elements == total_output_elements, \
+            'For Flatten module the total elements in input should equal the total output elements.'
+
+        super().__init__(ID, inputs, output)
+    
+
+    @override
+    def forward(self) -> None:
+        batch_size = self.inputs[0].val.shape[-1]
+
+        # Flatten the input into a 2D matrix of size (dim1, m) where dim1 is no. of elements in
+        # each training example and m is the batch size
+        self.output.val = self.inputs[0].val.reshape(-1, batch_size)
+
+
+    @override
+    def backward(self) -> None:
+        # Backward pass is simply reshaping the output derivative
+        self.inputs[0].deriv = self.output.deriv.reshape(self.inputs[0].val.shape)
