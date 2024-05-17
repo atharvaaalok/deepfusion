2ff5d9e0935364ca4f018eba9381cd31d72ae7a6
atharvaaalok <atharvaaalok@gmail.com>
2024-05-17 15:24:19 +0530

    Implemented batch normalization for convolutional inputs.

---
+++ Testing_generalized_norm.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+from .components.data import Data
+from .components.modules import Conv2D
+from .components.modules.activation_functions import Relu
+from .components.modules.normalizations.batch_norm_conv import BatchNorm
+from .components.modules.loss_functions import SumLoss
+from .components.net import Net
+from .utils.grad_check import gradient_checker
+from .conv_data import *
+
+
+np.set_printoptions(linewidth = np.inf)
+np.random.seed(0)
+
+
+# Get input and filter
+# Inputs X1, X2 and filters F1, F2 are in file conv_data.py which have been imported above
+n = 7
+f = 3
+padding = 0
+stride = 1
+filter_count = 2
+
+weight_init_type = 'He'
+
+# Construct neural network
+x = Data(ID = 'x', shape = (3, n, n))
+inputs = [x]
+
+o = ((n + 2 * padding - f) // stride) + 1
+z = Data(ID = f'z', shape = (filter_count, o, o))
+conv = Conv2D(ID = f'Conv2D', inputs = inputs, output = z, filter_size = f, 
+                filter_count = filter_count, padding = padding, stride = stride, weight_init_type = weight_init_type)
+
+z_norm = Data(ID = f'z_norm', shape = (filter_count, o, o))
+norm = BatchNorm(ID = f'Norm', inputs = [z], output = z_norm)
+
+a = Data(ID = 'a', shape = (filter_count, o, o))
+act_f = Relu(ID = 'Tanh', inputs = [z_norm], output = a)
+
+# Initialize, loss variable and attach loss function
+loss = Data(ID = 'loss', shape = (1, 1))
+sum_loss = SumLoss(ID = 'SumLoss', inputs = [a], output = loss)
+
+# Initialize neural network
+net = Net(ID = 'Net', root_nodes = [loss])
+
+
+# Combine the inputs X1 and X2 into a single input with first dimension at the batch dimension
+A = np.stack((X1, X2), axis = 0)
+
+# Set the input and the filters for the net
+x.val = A
+conv.parameter_list[0].val = F1
+conv.parameter_list[1].val = F2
+
+
+net.forward()
+print(z.val)
+
+net.backward()
+print(x.deriv.shape)
+
+
+gradient_checker(net = net, data_obj = x, loss_obj = loss, h = 1e-6)
--- components/modules/normalizations/batch_norm_conv.py
+++ components/modules/normalizations/batch_norm_conv.py
@@ -71,10 +71,10 @@ class BatchNorm(Module):
         # Go through checks first
         assert inputs[0].shape == output.shape, \
             'For BatchNorm input and output shape should be same.'
-
+        
         # Define the parameters gamma and beta associated with the module
-        gamma_shape = output.shape
-        beta_shape = output.shape
+        gamma_shape = (1, output.shape[0], 1, 1)
+        beta_shape = (1, output.shape[0], 1, 1)
         self.gamma = Data(ID = ID + '_gamma',
                           shape = gamma_shape,
                           val = np.ones(gamma_shape),
@@ -86,7 +86,7 @@ class BatchNorm(Module):
                          val = np.zeros(beta_shape),
                          is_frozen = is_frozen,
                          optimizer_details = optimizer_details)
-
+        
         parameter_list = [self.gamma, self.beta]
 
         super().__init__(ID, inputs, output, parameter_list = parameter_list,
@@ -105,7 +105,7 @@ class BatchNorm(Module):
         self.cache_test = {'mu_avg': 0, 'sigma_sq_avg': 0}
         # Maintain counter of update steps for calculating average of mean and var incrementally
         self.t = 0
-
+    
 
     @override
     def forward(self) -> None:
@@ -125,12 +125,12 @@ class BatchNorm(Module):
             self.t += 1
             self.cache_test['mu_avg'] += (mu - self.cache_test['mu_avg']) / self.t
             self.cache_test['sigma_sq_avg'] += (sigma_sq - self.cache_test['sigma_sq_avg']) / self.t
-        
+
         elif self.mode == 'test':
             # Use the cached average mean and variance from training
             mu = self.cache_test['mu_avg']
             sigma_sq = self.cache_test['sigma_sq_avg']
-
+        
         # Normalize X
         X_hat = (X - mu) / np.sqrt(sigma_sq + self.epsilon)
 
@@ -150,8 +150,8 @@ class BatchNorm(Module):
         X_hat = (X - mu) / np.sqrt(sigma_sq + self.epsilon)
 
         # Set derivatives for the parameters beta and gamma
-        self.beta.deriv = self.beta.deriv + np.sum(out_deriv, axis = 0, keepdims = True)
-        self.gamma.deriv = self.gamma.deriv + np.sum(out_deriv * X_hat, axis = 0, keepdims = True)
+        self.beta.deriv = self.beta.deriv + np.sum(out_deriv, axis = (0, 2, 3), keepdims = True)
+        self.gamma.deriv = self.gamma.deriv + np.sum(out_deriv * X_hat, axis = (0, 2, 3), keepdims = True)
 
         # Calculate derivative of loss w.r.t. X_hat
         dX_hat = self.gamma.val * out_deriv
@@ -162,8 +162,8 @@ class BatchNorm(Module):
         dX_3 = - X_hat * np.mean(X_hat * dX_hat, axis = 0, keepdims = True)
 
         self.inputs[0].deriv = self.inputs[0].deriv + (1 / np.sqrt(sigma_sq + self.epsilon)) * (dX_1 + dX_2 + dX_3)
-    
 
+    
     def set_mode(self, mode: str) -> None:
         """Sets the mode (training/testing) for the module.
         
