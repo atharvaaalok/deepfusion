d7121d5e58b87d49d0be7d3857a314537003e802
atharvaaalok <atharvaaalok@gmail.com>
2024-05-17 22:45:29 +0530

    Removed the separate FNN and CNN layer normalization modules and combined into one generalized module.

--- components/modules/normalizations/layer_norm.py
+++ components/modules/normalizations/layer_norm.py
@@ -64,10 +64,16 @@ class LayerNorm(Module):
         # Go through checks first
         assert inputs[0].shape == output.shape, \
             'For Layer Norm input and output shape should be same.'
-
+        
+        # Determine whether input is a vector or convolutional volume
+        if len(inputs[0].shape) == 3:
+            conv_input = True
+        else:
+            conv_input = False
+        
         # Define the parameters gamma and beta associated with the module
-        gamma_shape = output.shape
-        beta_shape = output.shape
+        gamma_shape = (1, output.shape[0], 1, 1) if conv_input else output.shape
+        beta_shape = (1, output.shape[0], 1, 1) if conv_input else output.shape
         self.gamma = Data(ID = ID + '_gamma',
                           shape = gamma_shape,
                           val = np.ones(gamma_shape),
@@ -90,7 +96,7 @@ class LayerNorm(Module):
         self.epsilon = 1e-8
 
         # Initialize cache which will store values used in forward pass that are needed for backprop
-        self.cache = {'mu': 0, 'sigma_sq': 0}
+        self.cache = {'conv_input': conv_input, 'mu': 0, 'sigma_sq': 0}
     
 
     @override
@@ -98,8 +104,9 @@ class LayerNorm(Module):
         X = self.inputs[0].val
 
         # Calculate mean and variance across features for each training example
-        mu = np.mean(X, axis = 1, keepdims = True)
-        sigma_sq = np.var(X, axis = 1, keepdims = True)
+        axes_to_norm = (1, 2, 3) if self.cache['conv_input'] else 1
+        mu = np.mean(X, axis = axes_to_norm, keepdims = True)
+        sigma_sq = np.var(X, axis = axes_to_norm, keepdims = True)
 
         # Cache mu and sigma_sq
         self.cache['mu'] = mu
@@ -111,8 +118,8 @@ class LayerNorm(Module):
 
         # Scale and shift X_hat to produce the output
         self.output.val = self.gamma.val * X_hat + self.beta.val
-
     
+
     @override
     def backward(self) -> None:
         out_deriv = self.output.deriv
@@ -125,15 +132,17 @@ class LayerNorm(Module):
         X_hat = (X - mu) / np.sqrt(sigma_sq + self.epsilon)
 
         # Set derivatives for the parameters beta and gamma
-        self.beta.deriv = self.beta.deriv + np.sum(out_deriv, axis = 0, keepdims = True)
-        self.gamma.deriv = self.gamma.deriv + np.sum(out_deriv * X_hat, axis = 0, keepdims = True)
+        axes_to_sum = (0, 2, 3) if self.cache['conv_input'] else 0
+        self.beta.deriv = self.beta.deriv + np.sum(out_deriv, axis = axes_to_sum, keepdims = True)
+        self.gamma.deriv = self.gamma.deriv + np.sum(out_deriv * X_hat, axis = axes_to_sum, keepdims = True)
 
         # Calculate derivative of loss w.r.t. X_hat
         dX_hat = self.gamma.val * out_deriv
 
         # Compute derivative of loss w.r.t. input X, which is sum of 3 parts
+        axes_to_norm = (1, 2, 3) if self.cache['conv_input'] else 1
         dX_1 = dX_hat
-        dX_2 = - np.mean(dX_hat, axis = 1, keepdims = True)
-        dX_3 = - X_hat * np.mean(X_hat * dX_hat, axis = 1, keepdims = True)
+        dX_2 = - np.mean(dX_hat, axis = axes_to_norm, keepdims = True)
+        dX_3 = - X_hat * np.mean(X_hat * dX_hat, axis = axes_to_norm, keepdims = True)
 
         self.inputs[0].deriv = self.inputs[0].deriv + (1 / np.sqrt(sigma_sq + self.epsilon)) * (dX_1 + dX_2 + dX_3)
