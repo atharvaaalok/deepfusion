ca0320b6f1ceffae35f5ae6de0f0b848c74ac3ec
atharvaaalok <atharvaaalok@gmail.com>
2024-05-17 11:11:54 +0530

    Added vanilla RNN modules!
    Instead of a single RNN module I broke it into 2 this falls into the current output is a single Data object paradigm and was easier to implement.

---
+++ Testing_rnn.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+from .components.data import Data
+from .components.modules.rnn import RNN1
+from .components.modules.rnn import RNN2
+from .components.modules.loss_functions import MSE
+from .components.net import Net
+from .utils.grad_check import gradient_checker
+
+
+def f(X):
+    Y = X[:, 0:1] + 2 * X[:, 1:2] ** 2 + 3 * X[:, 2:3] ** 0.5
+    return Y
+
+
+np.set_printoptions(linewidth = np.inf)
+np.random.seed(0)
+
+
+# Construct a 1 unit RNN
+m_train = 4
+a_train = np.zeros((m_train, 3))
+factor = 5
+X_train = np.random.rand(m_train, 3) * factor
+Y_train = f(X_train)
+
+
+# Construct the neural network
+a0 = Data(ID = 'a0', shape = (1, 3))
+x0 = Data(ID = 'x0', shape = (1, 3))
+
+a1 = Data(ID = 'a1', shape = (1, 3))
+rnn1 = RNN1(ID = 'RNN1', inputs = [a0, x0], output = a1)
+
+y1 = Data(ID = 'y1', shape = (1, 1))
+rnn2 = RNN2(ID = 'RNN2', inputs = [a1], output = y1)
+
+# Attach loss layer
+y = Data(ID = 'y', shape = (1, 1))
+loss = Data(ID = 'loss', shape = (1, 1))
+mse = MSE(ID = 'MSE', inputs = [y1, y], output = loss)
+
+
+# Initialize neural network
+net = Net(ID = 'Net', root_nodes = [loss])
+
+
+# # Visualize the network
+# net.visualize()
+
+
+# Set required values
+a0.val = a_train
+x0.val = X_train
+y.val = Y_train
+
+
+gradient_checker(net = net, data_obj = x0, loss_obj = loss, h = 1e-6)
---
+++ components/modules/rnn/rnn1.py
@@ -0,0 +1,124 @@
+from ....utils.backend import Backend
+np = Backend.get_array_module()
+
+from typing import Optional, override
+
+from ..module import Module
+from ...data.data import Data
+from ...optimizers import DEFAULT_OPTIMIZER_DETAILS
+from ..utils.functions import _sigmoid, _sigmoid_deriv
+
+
+class RNN1(Module):
+    """
+    """
+
+    different_at_train_test = False
+    is_regularizable = False
+
+
+    def __init__(
+        self,
+        ID: str,
+        inputs: list[Data],
+        output: Data,
+        learning_rate: float = 1e-6,
+        is_frozen: bool = False,
+        optimizer_details: dict = DEFAULT_OPTIMIZER_DETAILS,
+        weight_init_type: str = 'He',
+    ) -> None:
+        """Initializes RNN1 module based on ID, inputs, output and other optional parameters."""
+
+        # Go through checks first
+        assert inputs[0].shape == output.shape, \
+            'For RNN1 input and output activation shape should be same.'
+        
+        # Define the weight and bias parameters associated with the RNN1 module
+        Waa_shape = (inputs[0].shape[1], output.shape[1])
+        Wax_shape = (inputs[1].shape[1], output.shape[1])
+        ba_shape = (output.shape)
+
+        self.Waa = Data(ID = ID + '_Waa', shape = Waa_shape,
+                        val = _weight_initialization(weight_init_type, Waa_shape),
+                        is_frozen = is_frozen, optimizer_details = optimizer_details)
+        
+        self.Wax = Data(ID = ID + '_Wax', shape = Wax_shape,
+                        val = _weight_initialization(weight_init_type, Wax_shape),
+                        is_frozen = is_frozen, optimizer_details = optimizer_details)
+        
+        self.ba = Data(ID = ID + '_ba', shape = ba_shape, val = np.zeros(ba_shape),
+                       is_frozen = is_frozen, optimizer_details = optimizer_details)
+        
+        parameter_list = [self.Waa, self.Wax, self.ba]
+
+        super().__init__(ID, inputs = inputs, output = output, parameter_list = parameter_list,
+                         learning_rate = learning_rate, is_frozen = is_frozen,
+                         optimizer_details = optimizer_details)
+        
+        # Cache values calculated in the forward pass that will be useful in the backward pass
+        self.cache = {'temp_a': 0}
+    
+
+    @override
+    def forward(self) -> None:
+        temp_a = self.inputs[0].val @ self.Waa.val + self.inputs[1].val @ self.Wax.val + self.ba.val
+        a1 = _sigmoid(temp_a)
+
+        self.output.val = a1
+
+        self.cache['temp_a'] = temp_a
+
+
+    @override
+    def backward(self) -> None:
+        d_temp_a = _sigmoid_deriv(self.cache['temp_a']) * self.output.deriv
+
+        self.inputs[0].deriv = self.inputs[0].deriv + d_temp_a @ self.Waa.val.T
+        self.inputs[1].deriv = self.inputs[1].deriv + d_temp_a @ self.Wax.val.T
+
+        self.ba.deriv = self.ba.deriv + np.sum(d_temp_a, axis = 0, keepdims = True)
+        self.Waa.deriv = self.Waa.deriv + self.inputs[0].val.T @ d_temp_a
+        self.Wax.deriv = self.Wax.deriv + self.inputs[1].val.T @ d_temp_a
+
+
+
+def _weight_initialization(weight_init_type: str, W_shape: tuple[int, int]) -> None:
+    """Utility function to initialize and return a weight matrix based on specified initialization
+    method.
+
+    Args:
+        weight_init_type: String chosen from the available initialization choices.
+        W_shape: Shape of the weight matrix to be initialized.
+
+    Raises:
+        ValueError: If the specified weight initialization method is not available.
+    """
+    
+    available_init = ['Zero', 'Random', 'Xavier', 'He', 'Sparse']
+
+    match weight_init_type:
+        case 'Zero':
+            return np.zeros(W_shape)
+        
+        case 'Random':
+            return np.random.randn(*W_shape) * 0.01
+        
+        case 'Xavier':
+            return np.random.randn(*W_shape) * np.sqrt(1 / W_shape[0])
+        
+        case 'He':
+            return np.random.randn(*W_shape) * np.sqrt(2 / W_shape[0])
+        
+        case 'Sparse':
+            sparsity = 0.1
+            weights = np.zeros(W_shape)
+            total_weights = np.prod(W_shape)
+            non_zero_count = int(total_weights * sparsity)
+            non_zero_indices = np.random.choice(total_weights, non_zero_count, replace = False)
+            non_zero_values = 0.01 * np.random.randn(non_zero_count)
+            np.put(weights, non_zero_indices, non_zero_values)
+            return weights
+        
+        case _:
+            raise ValueError('Specified initialization method not available.' \
+                             f' Choose from - {available_init}')
