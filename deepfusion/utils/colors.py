322d5b9c5d24da0f89c25204de7a37ac582184fe
atharvaaalok <atharvaaalok@gmail.com>
2024-05-20 11:06:11 +0530

    Added a few more colors to the list of available colors for printing on terminal.

--- deepfusion/utils/colors.py
+++ deepfusion/utils/colors.py
@@ -1,4 +1,7 @@
 # Set colors that will be used to highlight the print output
-red = '\033[0;31m' # Red
-cyan = '\033[0;36m' # Cyan
 color_end = '\033[0m' # Reset terminal color
+red = '\033[0;31m'
+cyan = '\033[0;36m'
+green = '\033[0;92m'
+yellow = '\033[0;93m'
+blue = '\033[0;94m'
