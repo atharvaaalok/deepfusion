16cb83959148a1184535782e35a3243bc8f2e06a
atharvaaalok <atharvaaalok@gmail.com>
2024-05-22 11:27:07 +0530

    Added the typing extensions module to list of install_requires.

--- setup.py
+++ setup.py
@@ -13,13 +13,20 @@ setup(
     version = '0.0.0',
     url = 'https://github.com/atharvaaalok/deepfusion',
     license = 'MIT',
-    description = 'A highly modular and customizable deep learning framework!',
+    description = 'DeepFusion is a highly modular and customizable deep learning framework!',
     long_description = readme,
     long_description_content_type = 'text/markdown',
     packages = find_packages(),
-    python_requires = '>=3.12',
-    install_requires = ['numpy==1.26.4', 'matplotlib==3.8.4', 'setuptools==69.5.1'],
-    extras_require = ['graphviz==0.20.3', 'cupy_cuda12x==13.1.0'],
-    keywords = ['deepfusion', 'deep learning', 'neural networks', 'artificial intelligence', 'machine learning',
-                'model', 'optimization', 'backpropagation'],
+    install_requires = [
+        'numpy==1.26.4',
+        'matplotlib==3.8.4',
+        'setuptools==69.5.1',
+        'typing_extensions==4.11.0'
+    ],
+    extras_require = {
+        'visualization': ['graphviz==0.20.3'],
+        'gpu': ['cupy_cuda12x==13.1.0']
+    },
+    keywords = ['deepfusion', 'deep learning', 'neural networks', 'artificial intelligence',
+                'machine learning', 'model', 'optimization', 'backpropagation'],
 )
