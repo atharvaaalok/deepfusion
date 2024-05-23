230a74a9157c989d8a3d73b892ae947457a4b968
atharvaaalok <atharvaaalok@gmail.com>
2024-05-23 15:11:34 +0530

    Added missing __init__.py to deepfusion directory.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name = 'deepfusion',
     author = 'Atharva Aalok',
     author_email = 'atharvaaalok@gmail.com',
-    version = '0.0.2',
+    version = '0.0.3',
     url = 'https://github.com/atharvaaalok/deepfusion',
     license = 'MIT',
     description = 'DeepFusion is a highly modular and customizable deep learning framework!',
